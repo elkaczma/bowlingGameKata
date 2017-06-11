67ce32688572e1c1d577020cafcbb85d80a285a2
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-06-11 18:35:01 +0200

    test3 green

--- src/main/java/edu/elkaczma/bowlingGameKata/Game.java
+++ src/main/java/edu/elkaczma/bowlingGameKata/Game.java
@@ -3,12 +3,23 @@ package edu.elkaczma.bowlingGameKata;
 public class Game {
 
 	private int score = 0;
+	private int[] rolls = new int[21];
+	private int currentRoll = 0;
 	
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll++] = pins;
 	}
 
 	public int score() {
+		for (int rollIndex = 0; rollIndex < currentRoll; rollIndex++) {
+			if (rolls[rollIndex] + rolls[rollIndex+1] == 10) {
+				score += 10 + rolls[rollIndex+2];
+				rollIndex++;
+			} else {
+				score += rolls[rollIndex];
+			}
+		}
+		
 		return score;
 	}
 
