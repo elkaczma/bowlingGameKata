0d28e86afd9aedad8c8e62b3818dbc2119203fc8
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-06-11 18:00:24 +0200

    created test class

---
+++ src/test/java/edu/elkaczma/bowlingGameKata/BowlingGameTest.java
@@ -0,0 +1,15 @@
+package edu.elkaczma.bowlingGameKata;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void itCompiles() {
+		assertThat(true, equalTo(true));
+	}
+
+}
